e9d110cf833630632ec18bf982b88fc40c3915b9
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-03 06:59:13 +0530

    Maze

--- General-AI/search.js
+++ General-AI/search.js
@@ -13,26 +13,30 @@ class search{
     startingPos
     EndingPos
     current
-    actions = {
-        up: this.current[0] - 1,
-        down: this.current[0] + 1,
-        left: this.current[1] - 1,
-        right: this.current[1] + 1
-    }
+    ExploredList = []
+    NavigationList = []
+    // actions = {
+    //     up: this.current[0] - 1,
+    //     down: this.current[0] + 1,
+    //     left: this.current[1] - 1,
+    //     right: this.current[1] + 1
+    // }
 
     constructor(){
         this.lines = data.split("\n")
         this.rows = this.lines.length
         this.columns = this.lines[0].length
         this.matrix = this.lines.map(element => element.split(""))
+        console.log(this.rows, this.columns,this.matrix[0][0])
         this.initialParsing()
         this.current = this.startingPos
+        this.NavigationList.push(this.startingPos)
     }
 
     initialParsing(){
         for(let i=0;i<this.rows;i++){
-            for(let j=0;i<this.columns;i++){
-                if(this.matrix[i][j] == "A"){
+            for(let j=0;j<this.columns;j++){
+                if(this.matrix[i][j] == 'A'){
                     this.startingPos = [i,j]
                 }else if(this.matrix[i][j] == "B"){
                     this.EndingPos = [i,j]
@@ -42,8 +46,9 @@ class search{
     }
 
     search(){
-        console.log("works")
-    }
+        this.NavigationList.length == 0?console.log("No Solution"):
+            console.log("Works")
+}
 }
 
-let a = new search()//.search()
+let a = new search().search()
