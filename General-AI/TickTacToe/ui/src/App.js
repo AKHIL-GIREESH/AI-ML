4d94591eb7b52d2e6f2554dfe49c7f2bc7ab5374
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-08 09:04:46 +0530

    Tic Tac Toe

--- General-AI/TickTacToe/ui/src/App.js
+++ General-AI/TickTacToe/ui/src/App.js
@@ -8,10 +8,8 @@ function App() {
   let buttons = []
 
   const generateBoxes = () => {
-    for(let i=0;i<3;i++){
-      for(let j=0;j<3;j++){
+    for(let i=0;i<9;i++){
         buttons.push(<button className='Slots' onClick={() => setVal("X")}>{val}</button>)
-      }
     }
     return buttons
   }
