75f25b74704d9d2708bee221aa1b056c208992ec
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-08 17:32:09 +0530

    Tic Tac Toe

--- General-AI/TickTacToe/frontend/src/main.jsx
+++ General-AI/TickTacToe/frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.jsx'
+import App from './App.jsx'
 import './index.css'
 import TicTacToe from './TickTacToe.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TicTacToe />
+    <App />
   </React.StrictMode>,
 )
