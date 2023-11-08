fba6d6bd385bf2a139921d0c9d30dda063de33a7
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-08 13:23:46 +0530

    Tic Tac Toe

--- General-AI/TickTacToe/ui/src/App.js
+++ General-AI/TickTacToe/ui/src/App.js
@@ -7,16 +7,17 @@ function App() {
 
   //const [val,setVal] = useState("")
   const [box,setBox] = useState(boxx)
+  const [machineTurn,setMachineTurn]= useState(false)
   //let buttons = []
 
-  const modifyBox = (param) => {
+  const modifyBox = (param,unit) => {
     setBox(prevBox => {
       return prevBox.map(item => {
         if(item.id === param && item.value===" "){
-          console.log(item)
+          //setMachineTurn(state => !state)
             return {
               ...item,
-              value:"X"
+              value:unit
             }
           }else{
             return item
@@ -27,9 +28,41 @@ function App() {
 
   console.log(box)
   let buttons = box.map(item =>{ 
-    console.log(item)
-  return <button key={item.id} className='Slots' onClick={() => modifyBox(item.id)}>{item.value}</button>
+  return <div key={item.id} className='Slots' onClick={() => {
+    modifyBox(item.id,"X")
+    setMachineTurn(true)
+    }}>{item.value}</div>
 })
+
+let k=0;
+  let matrix = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+  //console.log(box[k])
+  for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
+      matrix[i][j] = box[k].value;
+      k++;
+    }
+  }
+ 
+const randomGenerator = () => {
+  return Math.floor(Math.random() * 2)
+}
+
+if(machineTurn){
+  let a = randomGenerator()
+  let b = randomGenerator()
+  while(matrix[a][b] !== ' '){
+      a = randomGenerator()
+      b = randomGenerator()
+  }
+  //matrix[a][b] = "O"
+  modifyBox(3*a+b,"O")
+  setMachineTurn(false)
+}
 
   return (
     <div className="App">
