a17916ad6c55108714dea9f196816c557b227433
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-08 10:10:41 +0530

    Tic Tac Toe

--- General-AI/TickTacToe/ui/src/App.js
+++ General-AI/TickTacToe/ui/src/App.js
@@ -1,24 +1,35 @@
 //import logo from './logo.svg';
 import './App.css';
 import {useState} from "react"
+import boxx from './box';
 
 function App() {
 
-  const [val,setVal] = useState("")
-  let buttons = []
+  //const [val,setVal] = useState("")
+  const [box,setBox] = useState(boxx)
+  //let buttons = []
 
-  const generateBoxes = () => {
-    for(let i=0;i<9;i++){
-        buttons.push(<button className='Slots' onClick={() => setVal("X")}>{val}</button>)
-    }
-    return buttons
+  const modifyBox = (param) => {
+    setBox(prevBox => {
+      return prevBox.map(item => {
+        console.log(item.val)
+        if(item.id === param){
+            return {
+              ...item,
+              val:"X"
+            }
+          }
+      })
+    })
   }
+
+  let buttons = box.map(item => <button key={item.id} className='Slots' onClick={() => modifyBox(item.id)}>{item.val}</button>)
 
   return (
     <div className="App">
       <h1>TICK-TAC-TOE</h1>
       <div className="Boxes">
-        {generateBoxes()}
+        {buttons}
       </div>
     </div>
   );
---
+++ General-AI/TickTacToe/ui/src/box.js
@@ -0,0 +1,39 @@
+const boxx = [
+    {
+        id:1,
+        val:" "
+    },
+    {
+        id:2,
+        val:" "
+    },{
+        id:3,
+        val:" "
+    },
+    {
+        id:4,
+        val:" "
+    },
+    {
+        id:5,
+        val:" "
+    },
+    {
+        id:6,
+        val:" "
+    },
+    {
+        id:7,
+        val:" "
+    },
+    {
+        id:8,
+        val:" "
+    },
+    {
+        id:9,
+        val:" "
+    }
+]
+
+export default boxx
