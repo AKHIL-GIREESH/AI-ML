5ce8c7e3b55f9fee5416f5e5cd4debcc1672e20e
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-11 23:26:07 +0530

    Tic Tac Toe

--- General-AI/TickTacToe/frontend/src/App.jsx
+++ General-AI/TickTacToe/frontend/src/App.jsx
@@ -2,14 +2,34 @@ import './App.css';
 import React, { useEffect, useState } from "react";
 
 function App() {
+  let terminationVal = { "X":1,"O":-1}
   const [matrix, setMatrix] = useState(() => {
     const initialMatrix = Array(3).fill(null).map(() => Array(3).fill(""));
     return initialMatrix;
   });
 
-  const Winner = () => {
-    console.log("Winner");
+  const Winner = (letter) => {
+    for(let i=0;i<3;i++){
+      if(state[i][0] == letter && state[i][1] == letter && state[i][2] == letter){
+          return terminationVal[letter]
+      }
+  }
+  for(let i=0;i<3;i++){
+      if(state[0][i] == letter && state[1][i] == letter && state[2][i] == letter){
+          return terminationVal[letter]
+      }
+  }
+
+  if(state[0][0] == letter && state[1][1] == letter && state[2][2] == letter){
+      return terminationVal[letter]
+  }
+  if(state[2][0] == letter && state[1][1] == letter && state[0][2] == letter){
+      return terminationVal[letter]
+  }
+  return 0;
   };
+
+  
 
   const modifyMatrix = (prop, unit) => {
     setMatrix((prevMatrix) => {
