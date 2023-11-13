b4c47c30f7bac6e1d5d0f43737e03c11ecf80e3c
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-13 19:13:28 +0530

    Greedy Search

--- General-AI/Search/GreedySearch.js
+++ General-AI/Search/GreedySearch.js
@@ -2,18 +2,68 @@ var fs = require("fs");
 
 var data = fs.readFileSync('pattern1').toString();
 
-console.log(data)
-lines = data.split("\n")
-rows = lines.length
-columns = lines[0].length
-matrix = lines.map(element => element.split(""))
+// console.log(data)
+// lines = data.split("\n")
+// rows = lines.length
+// columns = lines[0].length
+// matrix = lines.map(element => element.split(""))
 
-for(let i=0;i<rows;i++){
-    for(let j=0;j<columns;j++){
-        if(matrix[i][j] !== "#"){
-            matrix[i][j] = `${Math.abs(i-0)+Math.abs(j-5)}`
+// for(let i=0;i<rows;i++){
+//     for(let j=0;j<columns;j++){
+//         if(matrix[i][j] !== "#"){
+//             matrix[i][j] = `${Math.abs(i-0)+Math.abs(j-5)}`
+//         }
+//     }
+// }
+
+//console.log(matrix)
+
+
+
+class greedySearch{
+    steps
+    rows
+    columns
+    matrix
+    lines
+    startingPos
+    EndingPos
+    current
+    ExploredList = []
+    NavigationList = []
+
+    constructor(){
+        this.lines = data.split("\n")
+        this.rows = this.lines.length
+        this.columns = this.lines[0].length
+        this.matrix = this.lines.map(element => element.split(""))
+        this.initialParsing()
+        this.NavigationList.push(this.startingPos)
+        this.steps = 0
+    }
+
+    initialParsing(){
+        for(let i=0;i<this.rows;i++){
+            for(let j=0;j<this.columns;j++){
+                if(this.matrix[i][j] === 'A'){
+                    this.startingPos = [i,j]
+                }else if(this.matrix[i][j] === 'B'){
+                    this.EndingPos = [i,j]
+                }
+            }
+        }
+
+        console.log(this.EndingPos)
+        for(let i=0;i<this.rows;i++){
+            for(let j=0;j<this.columns;j++){
+                if(this.matrix[i][j] !== "#"){
+                    this.matrix[i][j] = `${Math.abs(this.EndingPos[0]-i)+Math.abs(this.EndingPos[1]-j)}`
+                }
+            }
         }
     }
 }
 
-console.log(matrix)
+const g = new greedySearch()
+
+console.log(g.matrix)
