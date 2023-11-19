974a493927afcae5f139499d88c751e6bb699b1b
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-19 22:26:55 +0530

    Knights and Knaves

--- General-AI/Logic-Engine/connectives.js
+++ General-AI/Logic-Engine/connectives.js
@@ -2,15 +2,25 @@ class Connectives{
 
     Symbols
     Knowledge
-    constructor(){
+    constructor(num){
         this.Symbols = []
         this.Knowledge = {}
+        this.initialSetUp(num)
+    }
+
+    Symbols(){
+    
     }
 
     checkKnowledge(){
         
     }
 
+    initialSetUp(num){
+        for(let i=0;i<2**num;i++){
+            console.log(i.toString(2).padStart(num, "0").split(""));
+        }
+    }
 
     AND(...a){
         return a.reduce((res,val) => res && val)
@@ -25,15 +35,15 @@ class Connectives{
     }
 
     IMPLIES(...a){
-        return a.reduce((res,val) => (val === false && res === true)?false:true)
+        return a.reduce((res,val) => (val == false && res == true)?false:true)
     }
 
     BICONDITIONAL(...a){
-        return a.reduce((res,val)=> res === val?true:false)
+        return a.reduce((res,val)=> res == val?true:false)
     }
 
 }
 
-let a = new Connectives()
+let a = new Connectives(3)
 a.AND(true,true,true)
-console.log(a.IMPLIES(false,false,false))
+console.log(a.IMPLIES(0,0,1))
