98950fa5b6e9c4d58e78f3c6f04bd91ee284de93
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-21 07:13:53 +0530

    Knights and Knaves

--- General-AI/Logic-Engine/connectives.js
+++ General-AI/Logic-Engine/connectives.js
@@ -4,16 +4,17 @@ class Connectives{
     Knowledge
     constructor(num){
         this.Symbols = []
-        this.Knowledge = []
+        this.Knowledge = Array(2**num).fill(null).map(() => Array(num).fill(""));
         this.initialSetUp(num)
     }
 
-    Symbols(){
-    
-    }
-
-    checkKnowledge(){
-        
+    getResult(){
+        console.log("Hi")
+        for(let i=0;i<2**3;i++){
+            if(parseInt(this.Symbols[i][0]) == this.Knowledge[i][0] && parseInt(this.Symbols[i][1]) == this.Knowledge[i][1] && parseInt(this.Symbols[i][2]) == this.Knowledge[i][2]){
+                console.log(this.Knowledge[i][0],this.Knowledge[i][1],this.Knowledge[i][2])
+            }
+        }
     }
 
     initialSetUp(num){
@@ -48,9 +49,13 @@ class Connectives{
 let a = new Connectives(3)
 
 for(let i=0;i<8;i++){
-    let t = a.Symbols[i]           
-    let ans = {P:t[0], Q:t[1], R:t[2]}
-    a.Knowledge[0].push(ans)
+    let t = a.Symbols[i]          
+    let ans = {P:t[0] === "0"?false:true, Q:t[1] === "0"?false:true, R:t[2] === "0"?false:true}
+    //console.log(ans.P,a.NOT(ans.P))
+    a.Knowledge[i][0] = a.OR(ans.P,a.NOT(ans.P))
+    a.Knowledge[i][1] = a.AND(a.NOT(ans.P),a.NOT(ans.R))
+    a.Knowledge[i][2] = ans.P
 }
+a.getResult()
 
 console.log(a.Knowledge)
