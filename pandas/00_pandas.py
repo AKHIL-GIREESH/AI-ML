fb5f58664d7bac64e489968e5a63e84d1154b14a
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-04 10:40:11 +0530

    Pandas

--- pandas/00_pandas.py
+++ pandas/00_pandas.py
@@ -2,4 +2,11 @@ import pandas as pd
 
 pokemonData = pd.read_csv("pokemon_data.csv")
 
-print(pokemonData)
+print(pokemonData.head(3))
+print("X---------------------X")
+
+print(pokemonData.columns)
+print("X---------------------X")
+
+print(pokemonData.iloc[1])
+print("X---------------------X")
