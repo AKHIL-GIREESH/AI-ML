7852d77ce5b4ac053dd0f3a803d2093ff8c5253a
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-26 12:09:49 +0530

    Spectro

--- pandas/SpectroFiltering.py
+++ pandas/SpectroFiltering.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import re
 
-attSheet3 = pd.read_excel("./attSheet3.xlsx", index_col=0)
+attSheet3 = pd.read_excel("./attSheet7.xlsx")
 
 print(attSheet3.columns)
 attSheet3 = attSheet3[['NAME','PHONE','COLLEGE NAME']].loc[~attSheet3['COLLEGE NAME'].str.contains('^f[a-z]*',flags=re.I,regex=True)]
 print(attSheet3)
+
+attSheet3.to_csv('MattSheet7.csv')
