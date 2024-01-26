5f177417ba6ca8f83d1fad1fb8d1dc279ba9b939
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-26 16:18:06 +0530

    Spectro

--- pandas/SpectroFiltering.py
+++ pandas/SpectroFiltering.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import re
 
-attSheet3 = pd.read_excel("./attSheet7.xlsx")
+attSheet3 = pd.read_csv("./Registration.csv")
 
-print(attSheet3.columns)
-attSheet3 = attSheet3[['NAME','PHONE','COLLEGE NAME']].loc[~attSheet3['COLLEGE NAME'].str.contains('^f[a-z]*',flags=re.I,regex=True)]
+attSheet3 = attSheet3[['NAME','PHONE','COLLEGE NAME']].iloc[130:]
+attSheet3 = attSheet3.drop_duplicates(subset='PHONE',keep='first')
 print(attSheet3)
 
-attSheet3.to_csv('MattSheet7.csv')
+# print(attSheet3.columns)
+# attSheet3 = attSheet3[['Name','Phone no','College']].loc[~attSheet3['College'].str.contains('^f[a-z]*',flags=re.I,regex=True)]
+# print(attSheet3)
+
+
+attSheet3.to_csv('MRegistration.csv')
