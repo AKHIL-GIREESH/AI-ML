3ca5ee501cb2423f2699ad260ef525f6905b032f
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-16 17:01:52 +0530

    LR

---
+++ DLAIC/Files/L2_LinearRegression.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_multi import  load_house_data, run_gradient_descent 
+from lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
+from lab_utils_common import dlc
+np.set_printoptions(precision=2)
