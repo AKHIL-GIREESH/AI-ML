6398827c0a610d86f9fff7f8658cda016f21ec7a
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-17 23:59:19 +0530

    Linear Regression

---
+++ DLAIC/Files/LinearRegressionScratch.py
@@ -0,0 +1,16 @@
+x = []
+w = 0
+b = 0
+
+
+def lineEqn(w,b,x):
+    pass
+
+def costFunction(w,b,x,y,m):
+    pass
+
+def gradientCalc(w,b,x,y,m,a):
+    pass
+
+def gradientDescent(w,b,x,y,m,a):
+    pass
