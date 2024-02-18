b093b126e7cf2905de7d9f3302fe63c4321e5272
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-18 16:50:17 +0530

    Linear Regression

--- DLAIC/Files/LinearRegressionScratch.py
+++ DLAIC/Files/LinearRegressionScratch.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def lineEqn(w,b,x):
-    return (w*x + b)
+    return (np.dot(w,x) + b)
 
 def costFunction(w,b,x,y,m):
     cost = 0
@@ -9,27 +9,32 @@ def costFunction(w,b,x,y,m):
         cost += (lineEqn(w,b,x[i]) - y[i])**2
     return (cost/(2*m))
 
-def gradientCalc(w,b,x,y,m):
-    dj_dw,dj_db = 0,0
+def gradientCalc(w, b, x, y, m):
+    dj_dw, dj_db = np.zeros(len(x[0])), 0
     for i in range(m):
-        dj_dw +=  (lineEqn(w,b,x[i]) - y[i])*x[i]
-        dj_dw +=  (lineEqn(w,b,x[i]) - y[i])
-    return (dj_dw/m),(dj_db/m)
+        prediction = lineEqn(w, b, x[i])
+        dj_db += (prediction - y[i])
+        for j in range(len(x[0])):
+            dj_dw[j] += (prediction - y[i]) * x[i][j]
+    return dj_dw, dj_db / m
+
 
 def gradientDescent(w,b,x,y,m,alpha,steps):
     for i in range(steps):
         print("Step = ",i,"Cost = ",costFunction(w,b,x,y,m))
         dj_dw,dj_db = gradientCalc(w,b,x,y,m)
-        w -= alpha*dj_dw
+        for j in range(len(x[0])):
+            w[j] -= alpha*dj_dw[j]/m
         b -= alpha*dj_db
+        
 
 
-x = np.array([1.0, 2.0])
-y = np.array([300.0, 500.0])
-w = 0
-b = 0
+x = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y = np.array([460, 232, 178])
+w = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+b = 785.1811367994083
 m = len(x)
-alpha = 0.01
+alpha = 0.0000001
 steps = 20
 
 gradientDescent(w,b,x,y,m,alpha,steps)
