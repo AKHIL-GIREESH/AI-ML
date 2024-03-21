5759e0caae9878ad1fc1a3fc98c1eed0205742ae
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-21 20:11:59 +0530

    NeuralNets

--- DLAIC/C2/NeuralNetsNumpy.py
+++ DLAIC/C2/NeuralNetsNumpy.py
@@ -4,20 +4,27 @@ def lineEqn(w,x,b):
     return np.dot(w,x)+b
 
 def sigmoid(w,x,b):
-    return 1/(1+np.e(-lineEqn(w,x,b)))
+    return 1/(1+np.exp(-lineEqn(w,x,b)))
 
 def Dense(W,a_in,b):
     a_out = np.zeros(W.shape[1])
     for i in range(W.shape[1]):
         w = W[:,i]
-        b = b[i]
-        a_out[i] = sigmoid(w,a_in,b)
+        _b = b[i]
+        a_out[i] = sigmoid(w,a_in,_b)
     return a_out
 
-def Sequence(W1,b1,W2,b2,a_in,):
+def Sequence(W1,b1,W2,b2,a_in):
+    a1 = Dense(W1,a_in,b1)
+    a2 = Dense(W2,a1,b2)
+    return a2
 
-    return
-
+def predict(W1,b1,W2,b2,a_in):
+    m = a_in.shape[0]
+    p = np.zeros((m,1))
+    for i in range(m):
+        p[i,0] = Sequence(W1,b1,W2,b2,a_in[i])
+    return p
 
 W1_tmp = np.array( [[-8.93,  0.29, 12.9 ], [-0.1,  -7.32, 10.81]] )
 b1_tmp = np.array( [-9.82, -9.28,  0.96] )
@@ -25,7 +32,7 @@ W2_tmp = np.array( [[-31.18], [-27.59], [-32.56]] )
 b2_tmp = np.array( [15.41] )
 
 X_tst = np.array([
-    [200,13.9],  
-    [200,17]])
+    [-0.47,0.42],  
+    [-0.47,3.16]])
 
-Dense(W1_tmp,b1_tmp,X_tst)
+predict(W1_tmp,b1_tmp,W2_tmp,b2_tmp,X_tst)
