0860a567f07cd3dd12b803d0f9b9d519390102c2
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-06-15 20:11:33 +0530

    Gemini

--- GenAI/geminiTut/index.js
+++ GenAI/geminiTut/index.js
@@ -1,12 +1,15 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
 require("dotenv").config();
 
 const genAI = new GoogleGenerativeAI(process.env.KEY);
 
-async function run() {
+//Text to Text
+
+async function Text2Text() {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 
-  const prompt = "Which is the best EV scooter company in India?";
+  const prompt = "INFJ and INTJ compatibility";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
@@ -14,4 +17,29 @@ async function run() {
   console.log(text);
 }
 
-run();
+//Img/Text to Text
+
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType,
+    },
+  };
+}
+
+async function Text_Img2Text() {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const prompt =
+    "Explain about the dude who draw this. Also brief about the painting itself";
+
+  const imageParts = [fileToGenerativePart("img1.jpeg", "image/jpeg")];
+
+  const result = await model.generateContent([prompt, ...imageParts]);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+//Text2Text();
+//Text_Img2Text();
