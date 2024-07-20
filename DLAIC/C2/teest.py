2b4fcf2e00e340a011c33b5398fde8d4edfa85e7
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-07-20 23:55:28 +0530

    nn back prop

--- DLAIC/C2/teest.py
+++ DLAIC/C2/teest.py
@@ -4,7 +4,7 @@ import numpy as np
 
 steps = 150
 A0 = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-Y = np.array([0, 1, 1, 0])
+Y = np.array([[0], [1], [1], [0]])
 
 X_test = np.array([[0, 0], [1, 1], [1, 0], [0, 1], [1, 1], [0, 1]])
 
@@ -40,18 +40,44 @@ def Cost(A):
     return cost
 
 
-def gradient(W2, B2, A2, A1):
-    dJ_dw = []
+def gradientRelu(Z):
+    return Z > 0
+
+
+def gradientSigmoid(A2, A1):
+
+    m = A1.shape[0]
+    n = A1.shape[1]
+
+    dJ_dw = np.zeros((n, 1))
     dJ_db = 0
-    m = A2.shape[0]
+
     for i in range(m):
-        for i in range():
-            dJ_dw[i] = (A2[i][0]-Y[i])*A1[i][0]  # Here
-    return
+        diff = A2[i]-Y[i][0]
+        dJ_db += diff
+        for j in range(n):
+            dJ_dw[j] += diff*A1[i][j]
+    return dJ_db/m, dJ_dw/m
 
 
-def gradientDescend(W1, B1, W2, B2, A1, A2, cost):
-    dW2 = gradient(W2, B2, A2, A1)
+def backpropagation(A1, A2, W1, W2, B1, B2, Z1):
+
+    dJ_dB2, dJ_dW2 = gradientSigmoid(A2, A1)
+    dJ_dZ2 = (Y-A2)/A2.shape[0]
+
+    dJ_dA1 = np.dot(dJ_dZ2, W2.T)
+    dJ_dZ1 = dJ_dA1 * gradientRelu(Z1)
+    dJ_dW1 = np.dot(A0.T, dJ_dZ1) / A2.shape[0]
+    dJ_dB1 = np.sum(dJ_dZ1, axis=0, keepdims=True) / A2.shape[0]
+
+    return dJ_dW1, dJ_dB1, dJ_dW2, dJ_dB2
+
+
+def gradientDescend(W1, B1, W2, B2, A1, A2, cost, Y, Z1):
+    backpropagation(A1, A2, W1, W2, B1, B2, Z1)
+
+    dJ_dA1 = 0
+    dJ_db2, dJdw2 = 0, 0
     return
 
 
@@ -73,7 +99,7 @@ def start():
     cost, Z1, A1, Z2, A2 = forwardProp(W1, B1, W2, B2)
     print(cost)
 
-    gradient(W1, B1, W2, B2, A1, A2, cost)
+    gradientDescend(W1, B1, W2, B2, A1, A2, cost, Y, Z1)
 
 
 start()
