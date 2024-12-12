38ecc8d210688dc61339b118d44b88ae9dac3bdc
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2024-12-12 23:55:02 +0530

    fromScratch logistic regression

---
+++ fromScratch/logisticRegression.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def lineEqn(w,b,x):
+    return
+
+def sigmoidFunc():
+    return
+
+def costFunc():
+    return 
+
+def gradientCalc():
+    return
+
+
+def gradientDescend(steps):
+    for i in range(steps):
+        print(f"Cost function @Step{i} = {costFunc()}")
+        gradientCalc() 
+    return
+
+
